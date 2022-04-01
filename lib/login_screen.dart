b41cec78b76d550661898334df945f6b6a42c21d
Jaymills <jaymillssamuel@gmail.com>
2022-04-01 03:31:11 +0000

    firstpush

---
+++ lib/login_screen.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+//import 'package:mozapp/signUp_screen.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(30),
+      child: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Text(
+                "SIGN IN",
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  labelText: "Enter email",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.email),
+                     fillColor: Color(0xfff3f3f4),
+                                      filled: true
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: "Enter Password",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.lock),
+                  suffixIcon: Icon(Icons.remove_red_eye),
+                     fillColor: Color(0xfff3f3f4),
+                                      filled: true
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                      onPressed: () {}, child: const Text("Forget Password?"))
+                ],
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Container(
+                height: 60,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  gradient: const LinearGradient(
+                      colors: [ Color(0xff14279B),
+                              Color(0xff14279B),
+                      ],),
+                ),
+                child: MaterialButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "LOGIN",
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Divider(
+                height: 30,
+                color: Colors.black,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an Account?",
+                    style: TextStyle(
+                      color: Colors.black.withOpacity(0.7),
+                    ),
+                  ),
+                  TextButton(
+                      onPressed: () {}, child: const Text("Register Account"))
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/signUp_screen.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+//import 'package:mozapp/signUp_screen.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(30),
+      child: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Text(
+                "SIGN UP",
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.name,
+                decoration: const InputDecoration(
+                  labelText: "Username",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.person_pin),
+                   fillColor: Color(0xfff3f3f4),
+                                      filled: true
+                ),
+              ),
+            
+              const SizedBox(
+                height: 25,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  labelText: "Enter email",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.email),
+                   fillColor: Color(0xfff3f3f4),
+                                      filled: true
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: "Enter Password",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.lock),
+                  suffixIcon: Icon(Icons.remove_red_eye),
+                   fillColor: Color(0xfff3f3f4),
+                                      filled: true
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: "Confirm Password",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.lock),
+                  suffixIcon: Icon(Icons.remove_red_eye),
+                   fillColor: Color(0xfff3f3f4),
+                                      filled: true
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                      onPressed: () {}, child: const Text("Forget Password?"))
+                ],
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Container(
+                height: 60,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  gradient: const LinearGradient(
+                      colors:[ Color(0xff14279B),
+                              Color(0xff14279B),
+                      ],
+                )),
+                child: MaterialButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "SIGN UP",
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Divider(
+                height: 30,
+                color: Colors.black,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an Account?",
+                    style: TextStyle(
+                      color: Colors.black.withOpacity(0.7),
+                    ),
+                  ),
+                  TextButton(
+                      onPressed: () {}, child: const Text("Register Account"))
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
